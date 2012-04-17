c9d8728a89df284a38cb1b895f59704a4d1d2f29
kcrayon <kcrayon@users.noreply.github.com>
2012-04-16 23:43:20 -0700

    First version.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-resque-jobs-per-worker
+resque-nofork
 ====
 
 Just like resque-jobs-per-fork, but without forking at all. Preform the job in the worker, and exit after a fixed number of jobs.
---
+++ lib/resque/plugins/nofork.rb
@@ -0,0 +1,28 @@
+require 'resque/worker'
+
+module Resque::Plugins
+  module Nofork
+    VERSION = "0.0.1"
+
+    def work_with_nofork(*args)
+      @cant_fork = true
+      work_without_nofork(*args)
+    end
+
+    def shutdown_with_nofork
+      shutdown_without_nofork || processed >= [ENV['JOBS_PER_WORKER'].to_i,1].max
+    end
+
+    def self.included(base)
+      base.instance_eval do
+        alias_method :work_wihout_nofork, :work
+        alias_method :work, :work_with_nofork
+
+        alias_method :shutdown_without_nofork, :shutdown?
+        alias_method :shutdown?, :shutdown_with_nofork
+      end
+    end
+  end
+
+  Resque::Worker.send(:include, Nofork)
+end
